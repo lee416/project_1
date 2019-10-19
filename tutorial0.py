f852d20240595d8636b66ab25138eb2e42584afe
lee416 <56602862+lee416@users.noreply.github.com>
2019-10-19 14:47:32 +0800

    test

---
+++ tutorial0.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 22:01:45 2019
+
+@author: user
+"""
+
+friends = ["kevin" , "Ama", "chr"]
+#                0       1       2
+#               -3      -2      -1
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+print(friends[-2])
+
+friends0 = ["kevin" , "Ama", "chr"]
+friends1 = ["amaaa" , "Ama", "chr"]
+
+friends.append(friends1)
+print(friends)
+
+
